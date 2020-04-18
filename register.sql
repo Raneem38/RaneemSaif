71076570bf131a63381fa2f1b36413d5c05b2353
Raneem38 <63890549+Raneem38@users.noreply.github.com>
2020-04-18 09:15:56 +0300

    Update register.sql

--- register.sql
+++ register.sql
@@ -37,6 +37,14 @@ CREATE TABLE `contacus` (
   `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `password` varchar(50) NOT NULL,
+ `trn_date` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+ );
 --
 -- Dumping data for table `contacus`
 --
